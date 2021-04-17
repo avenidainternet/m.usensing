8b5e742606ebc7a61903e137297376ebdacfaddf
avenidainternet <francisco.olivares.rodriguez@gmail.com>
2021-04-16 21:24:41 -0400

    6 sin reCapcha

--- js/usensing.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-  $scope.keysitio = "6LcgAK0aAAAAAIEhKXpBnr-veYtZ2pXkbqDL_LW4";
-  $scope.keysecreto = "6LcgAK0aAAAAAGxb4_eEqIpM3H04f0nnldrooWWG";
-});
